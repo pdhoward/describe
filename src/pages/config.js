a4d8cac70301bcde1bddca0b4699b1d442141e4b
pdhoward <patrick.howard@hotmail.com>
2018-09-05 08:45:25 -0400

    minor update to config

--- src/pages/config.js
+++ src/pages/config.js
@@ -3,7 +3,8 @@ import Link from "gatsby-link";
 
 export default () => (
   <div>
-    <p>Hello world from my second Gatsby page</p>
+    <p>Hello world from Strategic Machines</p>
+    <p>Placeholder to config your private messaging system</p>
     <Link to="/">back home</Link>
   </div>
 );
